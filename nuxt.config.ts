84fc3d9d6f50ed2b6a53075a46ae29779755b890
Ariel Callisaya <ariel.callisaya@trestristestigres.com>
2024-03-17 03:08:05 -0300

    test 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  
+  devServer: {
+    port: 3001,
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
